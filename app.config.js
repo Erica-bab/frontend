95e57e8b082a450e33ce318f52ebfc9e5c59d7b6
mokmokahr <mokmokahr@users.noreply.github.com>
2025-11-21 18:10:42 +0900

    feat: connect google maps api

---
+++ app.config.js
@@ -0,0 +1,48 @@
+export default {
+  expo: {
+    name: "front",
+    slug: "front",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/Group 2.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/app/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/app/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      },
+      package: "com.anonymous.front"
+    },
+    web: {
+      favicon: "./assets/app/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "react-native-maps",
+        {
+          googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      ]
+    ]
+  }
+};
