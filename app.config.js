48a5dc1162c7e287e872d721755d7e4bbcda43a1
손동열 <sdy@sondong-yeol-ui-MacBookPro.local>
2025-11-27 21:34:08 +0900

    chore: 앱 아이콘/config 변경

--- app.config.js
+++ app.config.js
@@ -1,20 +1,20 @@
 export default {
   expo: {
-    name: "front",
-    slug: "front",
+    name: "에푸",
+    slug: "efoo",
     version: "1.0.0",
     scheme: ["efoo", "com.googleusercontent.apps.1041029378289-puugfhcoucnpvmi8bk8k2a5uapiaak38"],
     extra: {
-      eas: {
-        projectId: "3536b605-1fa3-4649-aae3-4ccc299ae2f4"
+      "eas": {
+        "projectId": "bbf9506d-af02-4c2e-98df-81034e95805b"
       }
     },
     orientation: "portrait",
-    icon: "./assets/images/logo.png",
+    icon: "./assets/app/adaptive-icon.png",
     userInterfaceStyle: "light",
     newArchEnabled: true,
     splash: {
-      image: "./assets/app/splash-icon.png",
+      image: "./assets/app/adaptive-icon.png",
       resizeMode: "contain",
       backgroundColor: "#3072F0"
     },
@@ -22,8 +22,10 @@ export default {
       supportsTablet: true,
       bundleIdentifier: "com.efoo.app",
       usesAppleSignIn: true,
+      displayName: "에푸",
       infoPlist: {
-        NSLocationWhenInUseUsageDescription: "주변 맛집을 위치순으로 정렬하기 위해 위치 정보가 필요합니다."
+        NSLocationWhenInUseUsageDescription: "주변 맛집을 위치순으로 정렬하기 위해 위치 정보가 필요합니다.",
+        CFBundleDisplayName: "에푸"
       },
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
@@ -34,6 +36,7 @@ export default {
         foregroundImage: "./assets/app/adaptive-icon.png",
         backgroundColor: "#3072F0"
       },
+      label: "에푸",
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false,
       config: {
@@ -44,7 +47,7 @@ export default {
       package: "com.anonymous.front"
     },
     web: {
-      favicon: "./assets/app/favicon.png"
+      favicon: "./assets/app/adaptive-icon.png"
     },
     plugins: [
       "expo-router",
